2d6f7acfa5937c012d5a87db42d02ca6b8697def
TRAN VAN MINH <tranminh210920042509@gmail.com>
2025-08-21 14:10:44 +0700

    create_controller

---
+++ Project/Controllers/PersonController.cs
@@ -0,0 +1,13 @@
+namespace Project.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192faa56dbacb06a00fd4b7b5a90b89a885f32bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c583191482b35e09d068f49ba0625180f27e8f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
